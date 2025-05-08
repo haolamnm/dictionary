e942922fa033fbd3765773e9f71e6ff3028b3553
haolamnm <haolamnm@gmail.com>
2025-05-08 18:23:02 +0700

    Add: Allow startup with specified file path

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
-#include "app.hpp"
+#include <string>
 
-int main() {
-    App app("../data/words.csv");
+#include "app.hpp"
+#include "utility.hpp"
+
+int main(int argc, char *argv[]) {
+    if (argc > 2) {
+        log(Status::Info, "dictionary.exe [filepath]");
+        return -1;
+    }
+    std::string default_filepath = "../data/words.csv";
+    std::string filepath = argv[1] ? argv[1] : default_filepath;
+
+    App app(filepath);
     app.run();
+
     return 0;
 }
